5fea4826f06689b63e5239665004f453c9188134
AykutSarac <aykutsarac0@gmail.com>
2021-11-09 21:07:41 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/aykutsarac/proc-manager
+
+go 1.17
+
+require github.com/mitchellh/go-ps v1.0.0
+
+require github.com/manifoldco/promptui v0.9.0
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	golang.org/x/sys v0.0.0-20181122145206-62eef0e2fa9b // indirect
+)
---
+++ manager.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/manifoldco/promptui"
+	"github.com/mitchellh/go-ps"
+)
+
+type processType struct {
+	Name string
+	pid  int
+}
+
+func getProccess() []processType {
+	processList, err := ps.Processes()
+	if err != nil {
+		log.Println("ps.Processes() Failed, are you using windows?")
+	}
+
+	var procList []processType
+
+	for x := range processList {
+		var process ps.Process
+		process = processList[x]
+
+		item := processType{Name: process.Executable(), pid: process.Pid()}
+		procList = append(procList, item)
+	}
+
+	return procList
+}
+
+func confirmBack() {
+	options := []string{"Back", "Quit"}
+
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ . | green }}",
+		Active:   "  {{ . | cyan }}",
+		Inactive: "  {{ . }}",
+	}
+
+	searcher := func(input string, index int) bool {
+		option := options[index]
+		name := strings.Replace(strings.ToLower(option), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+
+	prompt := promptui.Select{
+		Label:     "Process killed successfully!",
+		Items:     options,
+		Templates: templates,
+		Searcher:  searcher,
+		Size:      2,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	if result == "Back" {
+		main()
+	} else {
+		os.Exit(0)
+	}
+}
+
+func confirmKill(pid int) {
+
+	options := []string{"Yes", "No"}
+
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ . | red }}",
+		Active:   "  {{ . | cyan }}",
+		Inactive: "  {{ . }}",
+	}
+
+	searcher := func(input string, index int) bool {
+		option := options[index]
+		name := strings.Replace(strings.ToLower(option), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+
+	prompt := promptui.Select{
+		Label:     "Are you sure you want to kill this process?",
+		Items:     options,
+		Templates: templates,
+		Searcher:  searcher,
+		Size:      2,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	if result == "Yes" {
+		proc, err := os.FindProcess(pid)
+
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			return
+		}
+
+		proc.Kill()
+		confirmBack()
+	} else {
+		main()
+	}
+}
+
+func main() {
+
+	procList := getProccess()
+
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ . |  green }}",
+		Active:   "▸ {{ .Name | cyan }}",
+		Inactive: "  {{ .Name }}",
+		Selected: "  {{ .Name | green }}",
+	}
+
+	searcher := func(input string, index int) bool {
+		process := procList[index]
+		name := strings.Replace(strings.ToLower(process.Name), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+
+	prompt := promptui.Select{
+		Label:     "Process to kill:",
+		Items:     procList,
+		Templates: templates,
+		Size:      8,
+		Searcher:  searcher,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	re := regexp.MustCompile("([0-9]+)")
+	procId := re.FindString(result)
+	pid, err := strconv.Atoi(procId)
+
+	confirmKill(pid)
+}
